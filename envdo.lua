c8375bedba8f59a7278d7d00f66e16c902c559e5
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-11-29 14:37:16 +0000

    envdo.lua: return enum for failure type

--- envdo.lua
+++ envdo.lua
@@ -15,16 +15,28 @@ end
 -- load each line from the file, setfenv on the resulting chunk, and return that.
 -- the caller then has to actually invoke it.
 -- label is optional but helps with diagnostics.
+local makeroset = function(list)
+	local result = {}
+	for _, v in ipairs(list) do result[v] = v end
+	return result
+end
+
+local e_iofail = "iofail"
+local e_loadfail = "loadfail"
+local enum = makeroset({e_iofail, e_loadfail})
+i.enum_loadfail = enum
+
 local envload = function(env, filename, label)
-	local file, err = io.open(filename, "r")
-	if file == nil then return nil, "io open fail: "..err end
-	local result, err = load(mk_preserve_newlines(file:lines()), label)
+	local result, err, message
+	local file, message = io.open(filename, "r")
+	if file == nil then return nil, e_iofail, message end
+	result, message = load(mk_preserve_newlines(file:lines()), label)
 	if result == nil then
-		err = "load fail: "..err
+		err = e_loadfail
 	else
 		setfenv(result, env)
 	end
-	return result, err
+	return result, err, message
 end
 i.load = envload
 
